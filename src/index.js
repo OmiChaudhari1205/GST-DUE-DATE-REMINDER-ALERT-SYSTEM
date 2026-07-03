c4a19174f2e5cce1838faa4c9c8c9af5e9cba979
dynamictrio78 <omichaudhari122005@gmail.com>
2026-07-03 18:34:26 +0530

    change in index.js file

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
 const PORT = 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`The Server running on port ${PORT}`);
 });
 
